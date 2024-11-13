bd4ce9bcdcc19a1e803b11fc58b48be5d17dc48c
Sebastian Lindgren <lindgrla@arcada.fi>
2024-11-13 10:55:33 +0200

    added script folder

---
+++ Assets/Scripts/Charactermovement.cs
@@ -0,0 +1,53 @@
+using System.Numerics;
+using UnityEngine;
+
+public class Charactermovement: MonoBehaviour
+{
+    UnityEngine.Vector2 Speed;  
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     Speed = new UnityEngine.Vector2(0, 0); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            //Varje frame ökar vektorer med Speed
+            transform.Translate(Speed * Time.deltaTime);
+            KeyboardController();
+            
+
+    }   
+
+    void KeyboardController()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {   
+            Speed.x = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Speed.x = -1;
+        }
+
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Speed.y = 1;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Speed.y = -1;
+        }
+
+        
+        
+        else
+        {
+            Speed.x = 0;
+            Speed.y = 0;
+        }
+
+    }
+}
